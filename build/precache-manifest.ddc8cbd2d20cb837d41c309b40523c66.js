e580f6941e170000d7b52c2485093af49fc0f838
craig91 <craigdejean1@gmail.com>
2020-02-17 09:47:39 -0500

    initial commit

---
+++ build/precache-manifest.ddc8cbd2d20cb837d41c309b40523c66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2c689ba7c365c026e7d62f16381b6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c21fba54fb88459b110a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b49ede963a0f2ff5b4ef",
+    "url": "/static/js/2.62dce5ae.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.62dce5ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21fba54fb88459b110a",
+    "url": "/static/js/main.27b07ed5.chunk.js"
+  },
+  {
+    "revision": "8963f78c0e1a10b5862f",
+    "url": "/static/js/runtime-main.8b07e775.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+
+class App extends Component {
+  constructor() {
+    super(); // calls the constructor method on the component class, thus giving access to this.state.
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
+  }
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+    return (
+      <div className="App">
+        
+        <input 
+          type='Search' 
+          placeholder='Search monsters' 
+          onChange={e => this.setState({searchField: e.target.value})}
+        />
+
+        <CardList monsters={filteredMonsters}/> 
+   
+      </div>
+    )
+  }
+}
+
+export default App;
+// for unique keys
+// The reason why we need those is because react needs to know what element it needs to update if one of the elements in the array has a value that changes.
