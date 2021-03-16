3abbbdb540ce64926bb0eff1c0e557e76cd0710c
craig91 <craigdejean1@gmail.com>
2021-03-16 02:04:44 -0400

    Fixed package vulnerabilities by updating some packages to latest versions

---
+++ build/precache-manifest.e53338241a97bec647ef90c9f491df25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d883856e166cdac298f985125dbb575",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "04eba2c571864d122e36",
+    "url": "/monsters-rolodex/static/css/main.e976cf52.chunk.css"
+  },
+  {
+    "revision": "56aae774d8f9eeb2cd6c",
+    "url": "/monsters-rolodex/static/js/2.0a529bf7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.0a529bf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04eba2c571864d122e36",
+    "url": "/monsters-rolodex/static/js/main.7c245a60.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ddc8cbd2d20cb837d41c309b40523c66.js"
+  "/monsters-rolodex/precache-manifest.e53338241a97bec647ef90c9f491df25.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters-rolodex/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
